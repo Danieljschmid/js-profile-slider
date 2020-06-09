693c2bad074472040aca221b88ba94e51332f0ee
Daniel Schmid <daniel@kodal.uk>
2020-06-09 16:31:07 +0100

    Updates

---
+++ precache-manifest.a8d9790534317612c86ddb9856312532.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55decebaa3f1fd58a0c367ef57f898d2",
+    "url": "/js-profile-slider/index.html"
+  },
+  {
+    "revision": "b4ff227ef6979946242e",
+    "url": "/js-profile-slider/static/css/main.2c171f92.chunk.css"
+  },
+  {
+    "revision": "0d30576cfaa11aedeb54",
+    "url": "/js-profile-slider/static/js/2.49066de5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-profile-slider/static/js/2.49066de5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ff227ef6979946242e",
+    "url": "/js-profile-slider/static/js/main.ba303a4f.chunk.js"
+  },
+  {
+    "revision": "7601044b5a05e9e0fa09",
+    "url": "/js-profile-slider/static/js/runtime-main.7a99bcbd.js"
+  },
+  {
+    "revision": "5421c364274a5e7333553851fc6b3714",
+    "url": "/js-profile-slider/static/media/icon-next.5421c364.svg"
+  },
+  {
+    "revision": "0fa4e8f2cf4223608fc878a70b8357af",
+    "url": "/js-profile-slider/static/media/icon-prev.0fa4e8f2.svg"
+  },
+  {
+    "revision": "9c1595060977e272ff1bea10380bfa6c",
+    "url": "/js-profile-slider/static/media/image-john.9c159506.jpg"
+  },
+  {
+    "revision": "343537c13a0f40dadbc3cca8d5ff30d1",
+    "url": "/js-profile-slider/static/media/image-tanya.343537c1.jpg"
+  },
+  {
+    "revision": "bb83e5bd104d93b264cae1bdfe037a4e",
+    "url": "/js-profile-slider/static/media/pattern-bg.bb83e5bd.svg"
+  },
+  {
+    "revision": "ae8c06393898e366f54e5fd28a9b3ae2",
+    "url": "/js-profile-slider/static/media/pattern-curve.ae8c0639.svg"
+  },
+  {
+    "revision": "83ba950c9ccf56bd0b8871f5007e0908",
+    "url": "/js-profile-slider/static/media/pattern-quotes.83ba950c.svg"
+  }
+]);
